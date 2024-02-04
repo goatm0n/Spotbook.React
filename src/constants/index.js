9d23fa6161ba538b7d1cf11a3bc0db8c7890109c
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2024-02-04 18:55:54 +0000

    use new backend url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://127.0.0.1:8000/";
+export const API_URL = "https://spotbookserver.spotbookapp.com/";
 export const ACCOUNTS_API_URL = API_URL + "accounts/api/";
 export const CLIPS_API_URL = API_URL + "clips/api/";
 export const SPOTS_API_URL = API_URL + "spots/api/";
